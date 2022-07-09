2555893765aab53dc9a6ec3aff1bb4b02b4ffa33
Alexander Khalivetov <haacarlhereagain@gmail.com>
2022-07-09 16:51:51 +0500

    Update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  publicPath: './'
+  publicPath: './',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Khalivetov Resume',
+    },
+  }
 }
